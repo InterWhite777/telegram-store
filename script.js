544f9ecd08b1e5f463a50beff85a4cf0a4f109b3
InterWhite777 <arsen.aubakirov.1988@mail.ru>
2025-04-16 14:09:37 +0500

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function buy(product, price) {
-  const data = { product, price };
-  Telegram.WebApp.sendData(JSON.stringify(data));
-  Telegram.WebApp.close();
+function buy(item, price) {
+  const data = {
+    item: item,
+    price: price
+  };
+
+  if (typeof Telegram !== 'undefined' && Telegram.WebApp) {
+    Telegram.WebApp.sendData(JSON.stringify(data));
+  } else {
+    alert("Telegram WebApp API не найден");
+  }
 }
