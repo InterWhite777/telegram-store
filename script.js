7d4cc76384a2f6aea99bd35469d8af9f016e0cfb
InterWhite777 <arsen.aubakirov.1988@mail.ru>
2025-04-18 12:11:43 +0500

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-function buy(productId, price) {
-  const tg = window.Telegram.WebApp;
-  const username = tg.initDataUnsafe.user.username || "без ника";
-  const payment_method = "YooMoney"; // по умолчанию
+let cart = [];
 
-  tg.sendData(`${productId}:${username}:${payment_method}`);
+function addToCart(button) {
+  const product = button.closest(".product");
+  const id = product.dataset.id;
+  const title = product.dataset.title;
+  const price = parseInt(product.dataset.price);
 
-  tg.close();
+  const index = cart.findIndex(item => item.id === id);
+  if (index >= 0) {
+    cart[index].quantity += 1;
+  } else {
+    cart.push({ id, title, price, quantity: 1 });
+  }
+
+  alert(`${title} добавлен в корзину`);
+}
+
+function checkout() {
+  if (cart.length === 0) {
+    alert("Корзина пуста");
+    return;
+  }
+
+  Telegram.WebApp.sendData(JSON.stringify(cart));
+  Telegram.WebApp.close();
 }
