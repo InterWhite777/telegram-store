fc4986ebbf14c7d09ccbdd9bceb7e78a70af36b9
InterWhite777 <arsen.aubakirov.1988@mail.ru>
2025-04-18 11:54:50 +0500

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
+function buy(productId, price) {
+  const tg = window.Telegram.WebApp;
+  const username = tg.initDataUnsafe.user.username || "без ника";
+  const payment_method = "YooMoney"; // по умолчанию
 
-function buy(product, price, paymentType = 'yoomoney') {
-  let user = window.Telegram.WebApp.initDataUnsafe?.user || {};
-  const data = {
-    товар: product,
-    сумма: price,
-    покупатель: user.username || user.first_name || 'неизвестен',
-    способ: paymentType
-  };
-  const message = JSON.stringify(data, null, 2);
+  tg.sendData(`${productId}:${username}:${payment_method}`);
 
-  Telegram.WebApp.sendData(message);
-
-  // Переход на оплату
-  if (paymentType === 'yoomoney') {
-    window.open(`https://yoomoney.ru/to/4100119106703740`, '_blank');
-  } else if (paymentType === 'qiwi') {
-    window.open(`https://qiwi.com/payment/form/99?extra%5B%27account%27%5D=YOUR_QIWI_NUMBER&amountInteger=${price}&currency=643&blocked[0]=account`, '_blank');
-  }
-
-  alert("✅ Заказ оформлен! Информация отправлена.");
+  tg.close();
 }
